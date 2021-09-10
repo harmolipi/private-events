7d045c0aa0a19c48cab5262b0b9ea4653c325628
harmolipi <nikob381@gmail.com>
2021-09-10 13:31:00 -0400

    Added creating and viewing events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,32 @@
 class EventsController < ApplicationController
+  # GET /events
   def index
     @events = Event.all
+  end
+
+  # GET /events/1
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  # GET /events/new
+  def new
+    @event = current_user.events.build
+  end
+
+  # POST /events
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :location, :creator_id)
   end
 end
