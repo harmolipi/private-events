809a5231bdf4ef1ce5aa65af07aa4f6dcd4e5ff3
harmolipi <nikob381@gmail.com>
2021-09-10 12:57:27 -0400

    Created users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users
   # get 'events/index'
   resources :events
+  resources :users
   root "events#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
