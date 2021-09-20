4d4f7f3ae8092fcf145b68efa7f542e0da2f17ee
harmolipi <nikob381@gmail.com>
2021-09-20 13:18:45 -0400

    Added check for authorization

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,8 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[show edit update destroy]
+  before_action :authorize_user, only: %i[edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+
   # GET /events
   def index
     @events = Event.all.sort_by(&:date)
@@ -6,12 +10,15 @@ class EventsController < ApplicationController
 
   # GET /events/1
   def show
-    @event = Event.find(params[:id])
   end
 
   # GET /events/new
   def new
     @event = current_user.events.build
+  end
+
+  # GET /events/1/edit
+  def edit
   end
 
   # POST /events
@@ -24,7 +31,30 @@ class EventsController < ApplicationController
     end
   end
 
+  # PATCH/PUT /events/1
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Event was successfully updated.'
+    else
+      render :edit, alert: "Event couldn't be updated. Please try again."
+    end
+  end
+
+  # DELETE /events/1
+  def destroy
+    @event.destroy
+    redirect_to root_path, notice: 'Event was successfully deleted.'
+  end
+
   private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def authorize_user
+    redirect_to root_path, alert: 'You are not authorized to edit this event.' unless @event.creator == current_user
+  end
 
   def event_params
     params.require(:event).permit(:title, :description, :date, :location, :creator_id)
