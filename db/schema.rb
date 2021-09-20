d6167b7069f18c32db9271ec55d1641081aea4cf
harmolipi <nikob381@gmail.com>
2021-09-20 11:38:37 -0400

    Properly configured username to save on signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_165041) do
+ActiveRecord::Schema.define(version: 2021_09_18_180351) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_09_15_165041) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
