73b1bc000cfaf99d0d3a0d415fd41966d0f17890
harmolipi <nikob381@gmail.com>
2021-09-21 13:43:49 -0400

    Added invitations routes and controller

---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,26 @@
+class InvitationsController < ApplicationController
+  def index
+    @users = User.all
+    @event = Event.find(params[:event_id])
+  end
+
+  def create
+    invitation = Invitation.new(event_id: invitation_params[:event_id], invitee_id: invitation_params[:invitee_id])
+    flash[:notice] = 'Something went wrong!' unless invitation.save
+    redirect_to invitations_path event_id: invitation_params[:event_id]
+  end
+
+  def destroy
+    invitation = Invitation.find(params[:id])
+    rsvp = Rsvp.find_by(attended_event_id: invitation.event_id, attendee_id: invitation.invitee_id)
+    invitation.destroy
+    rsvp.destroy
+    redirect_to invitations_path event_id: invitation.event_id
+  end
+
+  private
+
+  def invitation_params
+    params.permit(:event_id, :invitee_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :events
   resources :users
   resources :rsvps
+  resources :invitations
   root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
